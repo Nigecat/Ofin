13ee8718e2e91c4a94e788636dab5683639fa351
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-10 15:06:59 +1100

    Add tokenstream types

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -5,12 +5,15 @@ use crate::tokens::Token;
 use std::convert::TryFrom;
 use tokenstreamtoken::TokenStreamToken;
 
+type TokenStream = Vec<TokenStreamToken>;
+type TokenStreamReturn = Result<TokenStream, TokenizerError>;
+
 /// Fully tokenize an input string
 pub fn tokenize(input: &str, application: &mut Application) -> Result<Vec<Token>, TokenizerError> {
     let tokenstream: Vec<TokenStreamToken> = Vec::new();
 
     // TODO: Actual tokenizing
-    // Pass 1: Process line endings
+    // Pass 1: Process line endings and convert incoming string to a token stream
     // Pass 2: Tokenize single character tokens
     // Pass 3: Allocate variables in the symbol table
     // Pass 4: Tokenize everything else
