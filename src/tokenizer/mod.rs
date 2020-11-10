fd465f1bcc071ec11027e5d867c952b07f5f1617
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-10 14:42:05 +1100

    Change order of tokenizer

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -11,8 +11,8 @@ pub fn tokenize(input: &str, application: &mut Application) -> Result<Vec<Token>
 
     // TODO: Actual tokenizing
     // Pass 1: Process line endings
-    // Pass 2: Allocate variables in the symbol table
-    // Pass 3: Tokenize single character tokens
+    // Pass 2: Tokenize single character tokens
+    // Pass 3: Allocate variables in the symbol table
     // Pass 4: Tokenize everything else
 
     // If there are any characters left after processing, then we must have invalid syntax somewhere
