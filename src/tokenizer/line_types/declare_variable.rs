4ff67e4ba20267638d8f291e0dbf264db7403bd3
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-10 21:16:28 +1100

    Add string type

--- src/tokenizer/line_types/declare_variable.rs
+++ src/tokenizer/line_types/declare_variable.rs
@@ -18,7 +18,7 @@ pub fn declare_variable(line: &TokenStream, application: &mut Application) -> Li
         let eq_pos = line
             .iter()
             .position(|c| c == &TokenStreamToken::Token(Token::Equals));
-        // If there is no equals sign
+        // If there is an equals sign
         if let Some(eq_pos) = eq_pos {
             // Extract the name of the variable
             let name = tokenstream_to_string(&clean(line[3..eq_pos].to_vec()))?;
@@ -46,7 +46,38 @@ pub fn declare_variable(line: &TokenStream, application: &mut Application) -> Li
         && line[5] == TokenStreamToken::Char('g')
     {
         trace!("Detected string declaration");
-        return Ok(true);
+        // Get the position of the equals sign
+        let eq_pos = line
+            .iter()
+            .position(|c| c == &TokenStreamToken::Token(Token::Equals));
+
+        // If there is an equals sign
+        if let Some(eq_pos) = eq_pos {
+            // Extract the name of the variable
+            let name = tokenstream_to_string(&clean(line[6..eq_pos].to_vec()))?;
+            // Extract the value of the variable
+            let mut value = clean(line[eq_pos + 1..].to_vec());
+            // Make sure the text is surrounded in quotes
+            if value.len() < 2 {
+                return Err(TokenizerError::new("Invalid string assignment"));
+            }
+            if value.first().unwrap() == &TokenStreamToken::Token(Token::DoubleQuote)
+                && value.last().unwrap() == &TokenStreamToken::Token(Token::DoubleQuote)
+            {
+                // Remove the quotes
+                value.remove(0);
+                value.remove(value.len() - 1);
+            } else {
+                return Err(TokenizerError::new("String assignment must start and end with double quotes"));
+            }
+            let value = tokenstream_to_string(&value)?;
+
+            trace!("Declaring variable '{}' with value: {}", name, value);
+            let symbol = symbols::SString::new(&value).unwrap();
+            application.symbols.set(&name, Box::new(symbol));
+
+            return Ok(true);
+        }
     }
 
     Ok(false)
