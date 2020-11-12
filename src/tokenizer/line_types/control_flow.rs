c7a543092801de9b13838c5efff543244bb68023
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-12 11:30:53 +1100

    Run formatter

--- src/tokenizer/line_types/control_flow.rs
+++ src/tokenizer/line_types/control_flow.rs
@@ -1,35 +1,44 @@
 use super::super::TokenStreamToken;
 use super::{LineResult, LineResultType};
-use crate::tokens::Token;
 use crate::errors::TokenizerError;
+use crate::tokens::Token;
 use std::convert::TryFrom;
 
 /// Handle control flow lines in the form of `(if|while) Token::Expression`
 pub fn control_flow(line: &[TokenStreamToken]) -> LineResultType {
     if line[0] == 'i' && line[1] == 'f' {
-        let exp_pos = line.iter().position(|t| match t {
-            TokenStreamToken::Token(Token::Expression(_)) => true,
-            _ => false,
-        }).ok_or_else(|| TokenizerError::new("If statement missing expression"))?;
-        
+        let exp_pos = line
+            .iter()
+            .position(|t| match t {
+                TokenStreamToken::Token(Token::Expression(_)) => true,
+                _ => false,
+            })
+            .ok_or_else(|| TokenizerError::new("If statement missing expression"))?;
+
         let exp = Token::try_from(&line[exp_pos]).unwrap();
         let ex = match exp {
             Token::Expression(e) => Some(e),
             _ => None,
-        }.unwrap();
+        }
+        .unwrap();
 
         return Ok(LineResult::new(true, Some([Token::If(ex)].to_vec())));
-    } else if line[0] == 'w' && line[1] == 'h' && line[2] == 'i' && line[3] == 'l' && line[4] == 'e' {
-        let exp_pos = line.iter().position(|t| match t {
-            TokenStreamToken::Token(Token::Expression(_)) => true,
-            _ => false,
-        }).ok_or_else(|| TokenizerError::new("While loop missing expression"))?;
-        
+    } else if line[0] == 'w' && line[1] == 'h' && line[2] == 'i' && line[3] == 'l' && line[4] == 'e'
+    {
+        let exp_pos = line
+            .iter()
+            .position(|t| match t {
+                TokenStreamToken::Token(Token::Expression(_)) => true,
+                _ => false,
+            })
+            .ok_or_else(|| TokenizerError::new("While loop missing expression"))?;
+
         let exp = Token::try_from(&line[exp_pos]).unwrap();
         let ex = match exp {
             Token::Expression(e) => Some(e),
             _ => None,
-        }.unwrap();
+        }
+        .unwrap();
 
         return Ok(LineResult::new(true, Some([Token::While(ex)].to_vec())));
     }
