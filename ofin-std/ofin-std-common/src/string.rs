be032efa0b0101a600e165e82e02f091740b455b
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-12-06 16:03:04 +1100

    Run cargo fmt

--- ofin-std/ofin-std-common/src/string.rs
+++ ofin-std/ofin-std-common/src/string.rs
@@ -1,7 +1,7 @@
 //! Standard library string manipulation functions
 
 /// Check if a string starts with a pattern
-/// 
+///
 /// Returns `true` if the given pattern matches the start of the given string.  
 /// Returns `false` if it does not.
 pub fn starts_with(string: String, pattern: String) -> bool {
@@ -9,7 +9,7 @@ pub fn starts_with(string: String, pattern: String) -> bool {
 }
 
 /// Check if a string ends with a pattern
-/// 
+///
 /// Returns `true` if the given pattern matches the end of the given string.  
 /// Returns `false` if it does not.
 pub fn ends_with(string: String, pattern: String) -> bool {
