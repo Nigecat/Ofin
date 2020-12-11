6db03b3cce0988469cdaa5ef550bc764b278471a
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-12-11 17:37:06 +1100

    Create shortcuts in syntax rules

--- ofin/src/lexer/syntax.rs
+++ ofin/src/lexer/syntax.rs
@@ -21,16 +21,28 @@ pub fn syntax() -> Vec<TokenMatcher> {
     for token in tokens.iter() {
         let (name, data) = token;
         let data = toml::to_string(data).unwrap();
-        let data: OfinSyntaxData = toml::from_str(&data).unwrap();
+        let mut data: OfinSyntaxData = toml::from_str(&data).unwrap();
 
         trace!("Parsed syntax for token {:?} with data {:?}", name, data);
+
+        let mut mutator: Option<fn(&str) -> &str> = None;
+
+        if data.extractor == Some("copy".to_string()) {
+            data.extractor = Some(data.matcher.clone());
+        }
+
+        if data.replace_with == Some("self".to_string()) {
+            data.replace_with = Some("$1".to_string());
+            data.extractor = Some(data.matcher.clone());
+            mutator = Some(|s: &str| { s });
+        }
 
         matchers.push(TokenMatcher::new(
             name.to_string(),
             data.matcher,
             data.replace_with.unwrap_or_else(|| "".to_string()),
             data.extractor,
-            None,
+            mutator,
         ));
     }
 
