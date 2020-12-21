54b7468e0d877ccdb7c16145bdfcba4fbbd54a5a
Nigecat <nigel@nigelq.com>
2020-12-21 12:57:52 +1100

    Add proper cli parsing

---
+++ ofin/src/cli.rs
@@ -0,0 +1,31 @@
+use crate::util;
+use clap::{crate_authors, crate_version, Clap};
+
+#[derive(Clap, Debug)]
+#[clap(version = crate_version!(), author = crate_authors!())]
+pub struct Cli {
+    /// The script to run
+    pub script: String,
+    /// Clear the internal cache prior to running this script
+    #[clap(long)]
+    clear_cache: bool,
+}
+
+impl Cli {
+    pub fn read() -> Result<Self, String> {
+        let cli = Cli::parse();
+
+        // Ensure the script we were passed exists
+        if !util::path_exists(&cli.script) {
+            return Err(format!("{}: file not found", cli.script));
+        }
+
+        // Remove the cache if we need to
+        if cli.clear_cache {
+            std::fs::remove_dir_all(util::executable_dir().join("cache"))
+                .expect("Unable to remove cache directory");
+        }
+
+        Ok(cli)
+    }
+}
--- ofin/src/main.rs
+++ ofin/src/main.rs
@@ -1,7 +1,9 @@
 use std::io::prelude::*;
 use std::{env, fs, process};
+mod cli;
 #[allow(dead_code)]
 mod util;
+use cli::Cli;
 
 static RUSTC: &[u8] = include_bytes!(env!("RUSTC_LOCATION"));
 static STDLIB: &[u8] = include_bytes!(env!("STDLIB_LOCATION"));
@@ -11,33 +13,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Init our logger
     pretty_env_logger::init();
 
+    // Load our cli arguments
+    let cli: Cli = Cli::read()?;
+
     // Get the directory that this binary is contained in
     let dir = util::executable_dir();
     // Extract the rust compiler and our stdlib
     if !util::path_exists(dir.join("rustc.exe")) {
-        let mut file = fs::File::create(dir.join("rustc.exe")).unwrap();
-        file.write_all(RUSTC).unwrap();
+        let mut file = fs::File::create(dir.join("rustc.exe"))?;
+        file.write_all(RUSTC)?;
     }
     if !util::path_exists(dir.join("stdlib.rlib")) {
-        let mut file = fs::File::create(dir.join("stdlib.rlib")).unwrap();
-        file.write_all(STDLIB).unwrap();
+        let mut file = fs::File::create(dir.join("stdlib.rlib"))?;
+        file.write_all(STDLIB)?;
     }
 
-    if let Some(script_path) = env::args().nth(1) {
-        if let Ok(script) = fs::read_to_string(&script_path) {
-            let res = ofin::execute(script);
-            if let Err(err) = res {
-                eprintln!("{}", err);
-                process::exit(1);
-            } else {
-                process::exit(0);
-            }
-        } else {
-            eprintln!("{}: file not found", script_path);
-            process::exit(1);
-        }
-    } else {
-        eprintln!("usage: ofin [script]");
+    let script = fs::read_to_string(&cli.script)?;
+    let res = ofin::execute(script);
+    if let Err(err) = res {
+        eprintln!("{}", err);
         process::exit(1);
     }
+
+    Ok(())
 }
