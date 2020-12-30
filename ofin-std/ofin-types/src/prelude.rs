d482873ea26cb229e17bd8bf1947de4617629782
Nigecat <nigel@nigelq.com>
2020-12-30 11:41:01 +1100

    Rename stdlib 'create' functions to 'new'

--- ofin-std/ofin-types/src/prelude.rs
+++ ofin-std/ofin-types/src/prelude.rs
@@ -3,14 +3,14 @@ pub use super::Int as __int;
 pub use super::OfinString as __string;
 use super::*;
 
-pub fn __create_string<S: AsRef<str>>(string: S) -> OfinString {
+pub fn __new_string<S: AsRef<str>>(string: S) -> OfinString {
     OfinString::new(string)
 }
 
-pub fn __create_int(int: isize) -> Int {
+pub fn __new_int(int: isize) -> Int {
     Int::new(int)
 }
 
-pub fn __create_bool(bl: bool) -> Bool {
+pub fn __new_bool(bl: bool) -> Bool {
     Bool::new(bl)
 }
