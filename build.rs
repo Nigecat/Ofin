39ec47510f00d6e4f7308e59ae65aa23506f5168
Nigecat <nigel@nigelq.com>
2020-12-30 11:23:08 +1100

    Fix caching issues with standard library

--- build.rs
+++ build.rs
@@ -2,16 +2,28 @@ use std::path::Path;
 use std::process::Command;
 use std::{env, fs};
 use which::which;
+use glob::glob;
 
 fn main() {
+    // Rerun if our standard library has been modified
+    for entry in glob("ofin-std/**/*.rs").unwrap() {
+        if let Ok(path) = entry {
+            println!("cargo:rerun-if-changed={}", path.display());
+        }
+    }
+
+    // Build our standard library so the include_dir crate can access it
+    env::set_current_dir("ofin-std").unwrap();
+    let _ = Command::new("cargo")
+        .args(&["build", "--release"])
+        .output();
+
+    // Bundle the rust compiler for linking our standard library
+    let rustc = fs::canonicalize(which("rustc").unwrap()).unwrap();
+    let rustc = rustc.to_str().unwrap();
     let rustc = format!(
         "static RUSTC: &[u8] = include_bytes!({});",
-        snailquote::escape(
-            fs::canonicalize(which("rustc").unwrap())
-                .unwrap()
-                .to_str()
-                .unwrap()
-        ),
+        snailquote::escape(rustc),
     );
 
     let data = format!("{}", rustc);
@@ -22,10 +34,4 @@ fn main() {
     fs::write(&path, data).expect("unable to write static data to file");
 
     println!("cargo:rustc-env=STATIC_LOCATION={}", path.to_str().unwrap());
-
-    // Build our dependencies so the include_dir crate can access them
-    env::set_current_dir("ofin-std").unwrap();
-    let _ = Command::new("cargo")
-        .args(&["build", "--release", "--target-dir", "target"])
-        .output();
 }
--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -9,6 +9,9 @@ pub struct Cli {
     /// Clear the internal cache prior to running this script
     #[clap(long)]
     clear_cache: bool,
+    /// Clear the internal standard library cache prior to running this script
+    #[clap(long)]
+    clear_lib: bool,
 }
 
 impl Cli {
@@ -24,6 +27,12 @@ impl Cli {
         if cli.clear_cache && util::path_exists(util::executable_dir().join("cache")) {
             std::fs::remove_dir_all(util::executable_dir().join("cache"))
                 .expect("Unable to remove cache directory");
+        }
+
+        // Remove our standard library if we need to 
+        if cli.clear_lib && util::path_exists(util::executable_dir().join("lib")) {
+            std::fs::remove_dir_all(util::executable_dir().join("lib"))
+                .expect("Unable to remove standard lbirary directory");
         }
 
         Ok(cli)
