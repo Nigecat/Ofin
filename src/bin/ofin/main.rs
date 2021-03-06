95e571e4692bc978aaf2d239faf54ea28bfd0692
Nigecat <nigel@nigelq.com>
2021-03-06 19:07:25 +1100

    Shift panic handler to seperate function

--- src/bin/ofin/main.rs
+++ src/bin/ofin/main.rs
@@ -4,56 +4,55 @@ use std::{fs, panic};
 use structopt::StructOpt;
 use tracing::error;
 
+fn panic_handler(panic_info: &panic::PanicInfo) {
+    let info = panic_info.payload().downcast_ref::<&str>().unwrap_or(&"");
+    let location = panic_info
+        .location()
+        .map(|location| format!("from {}:{}", location.line(), location.file()))
+        .unwrap_or_default();
+
+    let repo = env!("CARGO_PKG_REPOSITORY");
+    let version = env!("CARGO_PKG_VERSION");
+    let date = ofin::build::BUILT_TIME_UTC;
+    let hash = ofin::build::GIT_COMMIT_HASH.unwrap_or("");
+    let target = ofin::build::TARGET;
+    let rustc = ofin::build::RUSTC_VERSION;
+
+    let msg = format!(
+        "
+        An internal panic has occured, this is a bug.
+        Please make a bug report at {repo} and include the following snippet:
+
+        v{version}
+        built on {date}
+        with hash {hash}
+        for target {target}
+        using {rustc}.
+
+        trace{location}:
+            {info}
+    ",
+        repo = repo,
+        version = version,
+        date = date,
+        hash = hash,
+        target = target,
+        rustc = rustc,
+        info = info,
+        location = location,
+    );
+
+    if tracing::dispatcher::has_been_set() {
+        error!("{}", msg);
+    } else {
+        println!("error: {}", msg);
+    }
+}
+
 fn main() {
     tracing_subscriber::fmt::init();
 
-    panic::set_hook(Box::new(|panic_info| {
-        let info = panic_info.payload().downcast_ref::<&str>().unwrap_or(&"");
-        let location = panic_info
-            .location()
-            .map(|location| format!("from {}:{}", location.line(), location.file()))
-            .unwrap_or_default();
-
-        let repo = env!("CARGO_PKG_REPOSITORY");
-        let version = env!("CARGO_PKG_VERSION");
-        let authors = env!("CARGO_PKG_AUTHORS");
-        let date = ofin::build::BUILT_TIME_UTC;
-        let hash = ofin::build::GIT_COMMIT_HASH.unwrap_or("");
-        let target = ofin::build::TARGET;
-        let rustc = ofin::build::RUSTC_VERSION;
-
-        let msg = format!(
-            "
-            An internal panic has occured, this is a bug.
-            Please make a bug report at {repo} and include the following snippet:
-
-            v{version}
-            created by {authors} and 
-            built on {date}
-            with hash {hash}
-            for target {target}
-            using {rustc}.
-
-            trace{location}:
-                {info}
-        ",
-            repo = repo,
-            version = version,
-            authors = authors,
-            date = date,
-            hash = hash,
-            target = target,
-            rustc = rustc,
-            info = info,
-            location = location,
-        );
-
-        if tracing::dispatcher::has_been_set() {
-            error!("{}", msg);
-        } else {
-            println!("error: {}", msg);
-        }
-    }));
+    panic::set_hook(Box::new(panic_handler));
 
     let args = Cli::from_args();
 
