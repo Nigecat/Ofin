fbd4ab4f5f84ea1ca199f73de03ffcebec9bfc30
Nigecat <nigel@nigelq.com>
2021-03-17 18:53:28 +1100

    Add custom panic handler

--- src/bin/ofin/main.rs
+++ src/bin/ofin/main.rs
@@ -1,9 +1,14 @@
 mod build;
 mod cli;
+mod panic;
 use cli::Cli;
 
 fn main() {
+    panic::set_handler();
+
     tracing_subscriber::fmt::init();
 
     let _cli = Cli::from_args();
+
+    assert_eq!(1, 2);
 }
---
+++ src/bin/ofin/panic.rs
@@ -0,0 +1,55 @@
+use crate::build;
+use std::panic;
+
+/// Set the custom panic handler
+pub fn set_handler() {
+    panic::set_hook(Box::new(panic_handler));
+}
+
+pub fn panic_handler(panic_info: &panic::PanicInfo) {
+    let info = panic_info.payload().downcast_ref::<&str>().unwrap_or(&"");
+    let location = panic_info
+        .location()
+        .map(|location| format!(" from {}:{}", location.file(), location.line()))
+        .unwrap_or_default();
+
+    let repo = env!("CARGO_PKG_REPOSITORY");
+    let version = env!("CARGO_PKG_VERSION");
+    let date = build::BUILT_TIME_UTC;
+    let hash = build::GIT_COMMIT_HASH.unwrap_or("");
+    let target = build::TARGET;
+    let rustc = build::RUSTC_VERSION;
+
+    let msg = format!(
+        "
+An internal panic has occured, this is a bug.
+Please make a bug report at {repo} and include the following snippet:
+
+---
+
+v{version}
+built on {date}
+with hash {hash}
+for target {target}
+using {rustc}.
+{info}
+trace{location}:
+{trace}
+    ",
+        repo = repo,
+        version = version,
+        date = date,
+        hash = hash,
+        target = target,
+        rustc = rustc,
+        info = info,
+        trace = panic_info,
+        location = location,
+    );
+
+    if tracing::dispatcher::has_been_set() {
+        tracing::error!("{}", msg);
+    } else {
+        println!("error: {}", msg);
+    }
+}
