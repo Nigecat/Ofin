6c28eafef6b3b3933b8d9855d0dece95b3859655
Nigecat <nigel@nigelq.com>
2021-03-28 14:11:09 +1100

    Implement operator traits for Number type

--- lib/src/types/number.rs
+++ lib/src/types/number.rs
@@ -1,4 +1,13 @@
-#[derive(Debug, Copy, Clone, Hash)]
+use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+// FIXME: Fix negative inequality (and add unit tests for it)
+
+/// A number.
+/// 
+/// TODO
+#[allow(clippy::derive_hash_xor_eq)] // We do not want the rounding behaviour of 2.0 == 2 to appear in the hashed value
+#[derive(Debug, Copy, Clone, Hash, Eq, Ord, PartialOrd)]
 pub struct Number {
     /// The numbers before the decimal point
     integral: i64,
@@ -8,15 +17,32 @@ pub struct Number {
     offset: Option<u32>,
 }
 
-impl Number {
-    /// Check if this number has no fractional part
-    fn is_num(&self) -> bool {
-        self.fractional == None
-    }
+/// The relation of the types between two numbers
+enum Relation {
+    /// Number, Number
+    NN,
+    /// Number, Float
+    NF,
+    /// Float, Number
+    FN,
+    /// Float, Float
+    FF,
+}
 
-    /// Check if this number has a fractional part (and is thus a float)
-    fn is_float(&self) -> bool {
-        self.fractional != None
+impl Number {
+    /// Detect the relation of the types between two numbers
+    fn detect(a: Self, b: Self) -> Relation {
+        if a.is_num() && b.is_num() {
+            Relation::NN
+        } else if a.is_num() && b.is_float() {
+            Relation::NF
+        } else if a.is_float() && b.is_num() {
+            Relation::FN
+        } else if a.is_float() && b.is_float() {
+            Relation::FF
+        } else {
+            unreachable!();
+        }
     }
 
     /// Create a number from a signed 64 bit integer
@@ -43,16 +69,91 @@ impl Number {
         }
     }
 
+    /// Check if this number has no fractional part
+    fn is_num(&self) -> bool {
+        self.fractional == None
+    }
+
+    /// Check if this number has a fractional part (and is thus a float)
+    fn is_float(&self) -> bool {
+        self.fractional != None
+    }
+
     /// Convert this number into a signed 64 bit integer
-    fn into_num(&self) -> i64 {
+    fn into_num(self) -> i64 {
         self.integral
     }
 
     /// Convert this number into a 64 bit float
-    fn into_float(&self) -> f64 {
+    fn into_float(self) -> f64 {
         match self.fractional {
             Some(fractional) => fractional as f64 / 10_u64.pow(self.offset.unwrap()) as f64,
             None => self.integral as f64,
+        }
+    }
+}
+
+// Implement comparison traits
+impl PartialEq for Number {
+    fn eq(&self, other: &Self) -> bool {
+        if self.integral == other.integral {
+            match Number::detect(*self, *other) {
+                Relation::NN => true,
+                Relation::FF => self.fractional == other.fractional && self.offset == other.offset,
+                // Allow for 2 to equal 2.0
+                Relation::NF => other.fractional == Some(0),
+                Relation::FN => self.fractional == Some(0),
+            }
+        } else {
+            false
+        }
+    }
+}
+
+// Implement the operator trait for $trait using operator $op
+macro_rules! _impl_op {
+    ($trait: ident, $f: ident, $op: tt) => {
+        impl $trait for Number {
+            type Output = Self;
+
+            fn $f(self, other: Self) -> Self {
+                // Perform the conversion on the integral
+                let integral = self.integral $op other.integral;
+
+                let fractional = match Number::detect(self, other) {
+                    Relation::NN => 0_f64,
+                    Relation::FN => self.fractional.unwrap() as f64 / 10_u64.pow(self.offset.unwrap()) as f64,
+                    Relation::NF => other.fractional.unwrap() as f64 / 10_u64.pow(other.offset.unwrap()) as f64,
+                    Relation::FF => {
+                        let fractional = self.fractional.unwrap() $op other.fractional.unwrap();
+                        fractional as f64 / 10_u64.pow(self.offset.unwrap()) as f64
+                    }
+                };
+
+                if fractional == 0_f64 {
+                    Number::from(integral)
+                } else {
+                    Number::from(integral as f64 + fractional)
+                }
+            }
+        }
+    };
+}
+
+// Implement operator traits
+_impl_op!(Add, add, +);
+_impl_op!(Sub, sub, -);
+_impl_op!(Mul, mul, *);
+_impl_op!(Div, div, /);
+
+impl Neg for Number {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Number {
+            integral: -self.integral,
+            fractional: self.fractional,
+            offset: self.offset,
         }
     }
 }
